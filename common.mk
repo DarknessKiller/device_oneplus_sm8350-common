6820bfc9b3f58408539b768dc361615fcfe007b9
DarknessKiller <23330152+DarknessKiller@users.noreply.github.com>
2023-05-12 09:18:29 +0000

    Revert "sm8350-common: Build KProfiles"
    This reverts commit 68739a1e3d05660f867d52b3dbe0bfa3e81c910e.

--- common.mk
+++ common.mk
@@ -243,10 +243,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.keymaster@4.1.vendor
 
-# KProfiles
-PRODUCT_PACKAGES += \
-    KProfiles
-
 # LiveDisplay
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay@2.1-service.oplus
